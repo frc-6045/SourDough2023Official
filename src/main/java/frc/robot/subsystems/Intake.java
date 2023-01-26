1f155efeb5d9a91db9894e4232897f6d1a8b7652
25habegab <117953064+25habegab@users.noreply.github.com>
2023-01-26 15:36:30 -0600

    Create Intake.java

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,74 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxLimitSwitch;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
+
+/*spark max motor controler  
+dont to create motor objects
+encoders
+*/
+public class Intake extends SubsystemBase{
+ 
+  public final CANSparkMax leftIntakeMotor;
+  public final CANSparkMax rightIntakeMotor;
+  public final MotorControllerGroup intakeMotorGroup;
+
+ RelativeEncoder leftIntakeEncoder;
+ RelativeEncoder rightIntakeEncoder;
+private int position;
+private double speed;
+public Intake()
+{
+    leftIntakeMotor = new CANSparkMax(ArmConstants.leftIntakeMotorCANID, MotorType.kBrushless);
+    rightIntakeMotor = new CANSparkMax(ArmConstants.rightIntakeMotorCANID, MotorType.kBrushless);
+   leftIntakeMotor.restoreFactoryDefaults();
+   rightIntakeMotor.restoreFactoryDefaults();
+   
+   
+   
+   
+  
+leftIntakeMotor.setInverted(true);
+   rightIntakeMotor.setInverted(true);
+
+intakeMotorGroup = new MotorControllerGroup(leftIntakeMotor, rightIntakeMotor);
+
+
+ leftIntakeEncoder = leftIntakeMotor.getEncoder();
+     rightIntakeEncoder = rightIntakeMotor.getEncoder();
+
+
+leftIntakeEncoder.setPosition(0);
+rightIntakeEncoder.setPosition(0);
+leftIntakeMotor.setInverted(true);
+} 
+
+
+    public void setSpeed(double speed)
+    {
+        leftIntakeMotor.set(speed);
+        rightIntakeMotor.set(speed);
+    }
+
+
+    @Override 
+    public void periodic() {
+
+
+
+
+    }
+
+}
+    
